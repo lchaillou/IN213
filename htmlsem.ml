de9ded1bf9888c67e417dbcf56015da31153732f
laetitia <laetitia.chaillou@ensta-paristech.fr>
2020-05-12 10:39:19 +0200

    sémantique opérationnelle

--- htmlsem.ml
+++ htmlsem.ml
@@ -40,58 +40,11 @@ let rec eval e rho =
   )
   | Ebr -> Brval
   | Eh1 (e) -> let e_val = eval e rho in Balval { balise = "h1" ; body = e_val ; env = rho }
-      
+  | Eh2 (e) -> let e_val = eval e rho in Balval { balise = "h2" ; body = e_val ; env = rho }
+  | Ep (e) -> let e_val = eval e rho in Balval { balise = "p" ; body = e_val ; env = rho }
+  | Ediv (e) -> let e_val = eval e rho in Balval { balise = "div" ; body = e_val ; env = rho }
+  | Estrong (e) -> let e_val = eval e rho in Balval { balise = "strong" ; body = e_val ; env = rho }
 
-
-  (* | EIdent v -> lookup v rho
-  | EApp (e1, e2) -> (
-      match (eval e1 rho, eval e2 rho) with
-      | (Funval { param; body; env }, v2) ->
-          let rho1 = extend env param v2 in
-          eval body rho1
-      | (Funrecval { fname; param; body; env } as fval, v2) ->
-          let rho1 = extend env fname fval in
-          let rho2 = extend rho1 param v2 in
-          eval body rho2
-      | (_, _) -> error "Apply a non-function"
-     )
-  | EMonop ("-", e) -> (
-      match eval e rho with
-      | Intval n -> Intval (-n)
-      | _ -> error "Opposite of a non-integer"
-     )
-  | EMonop (op, _) -> error (Printf.sprintf "Unknown unary op: %s" op)
-  | EBinop (op, e1, e2) -> (
-      match (op, eval e1 rho, eval e2 rho) with
-      | ("+", Intval n1, Intval n2) -> Intval (n1 + n2)
-      | ("-", Intval n1, Intval n2) -> Intval (n1 - n2)
-      | ("*", Intval n1, Intval n2) -> Intval (n1 * n2)
-      | ("/", Intval n1, Intval n2) -> Intval (n1 / n2)
-      | (("+"|"-"|"*"|"/"), _, _) ->
-          error "Arithmetic on non-integers"
-      | ("<",  Intval n1, Intval n2) -> Boolval (n1 < n2)
-      | (">",  Intval n1, Intval n2) -> Boolval (n1 > n2)
-      | ("=",  Intval n1, Intval n2) -> Boolval (n1 = n2)
-      | ("<=", Intval n1, Intval n2) -> Boolval (n1 <= n2)
-      | (">=", Intval n1, Intval n2) -> Boolval (n1 >= n2)
-      | (("<"|">"|"="|"<="|">="), _, _) ->
-          error "Comparison of non-integers"
-      | _ -> error (Printf.sprintf "Unknown binary op: %s" op)
-     )
-  | EIf (e, e1, e2) -> (
-      match eval e rho with
-      | Boolval b -> eval (if b then e1 else e2) rho
-      | _ -> error "Test on a non-boolean"
-     )
-  | EFun (a, e) -> Funval { param = a; body = e; env = rho }
-  | ELet (x, e1, e2) ->
-      let v1 = eval e1 rho in
-      let rho1 = extend rho x v1 in
-      eval e2 rho1
-  | ELetrec (f, x, e1, e2) ->
-      let fval = Funrecval { fname = f; param = x; body = e1; env = rho } in
-      let rho1 = extend rho f fval in
-      eval e2 rho1 *)
 ;;
 
 
